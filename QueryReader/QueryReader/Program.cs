be38a642c1c845eadda7c05397e8ccfd2bbabba0
jesusBalam <jesus.balam@hiumanlab.com>
2022-07-06 10:59:29 -0500

    Handle exceptions
    Handle exceptions

--- QueryReader/QueryReader/Program.cs
+++ QueryReader/QueryReader/Program.cs
@@ -10,22 +10,30 @@ namespace QueryReader
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Press any key to start.");
-            Console.ReadKey();
-
-            Console.WriteLine("Paste file path to read: (Space/Enter to take default path in config)");
-            string pathFile = Console.ReadLine();
-            pathFile.Trim();
-
-            if(!string.IsNullOrEmpty(pathFile))
+        {            
+            try
             {
-                ConfigurationManager.AppSettings["FileInput"] = pathFile;
-            }
+                Console.WriteLine("Press any key to start.");
+                Console.ReadKey();
 
-            QueryReader.ReadFile(@ConfigurationManager.AppSettings["FileInput"]);
-            Console.WriteLine("Process Done.");
-            Console.ReadKey();
+                Console.WriteLine("Paste file path to read: (Space/Enter to take default path in config)");
+                string pathFile = Console.ReadLine();
+                pathFile.Trim();
+
+                if (!string.IsNullOrEmpty(pathFile))
+                {
+                    ConfigurationManager.AppSettings["FileInput"] = pathFile;
+                }
+                QueryReader.ReadFile(@ConfigurationManager.AppSettings["FileInput"]);
+                Console.WriteLine("Process Done.");
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }            
         }
     }
 }
